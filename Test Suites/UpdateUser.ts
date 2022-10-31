ba69bba86d84a661d216d77d9f05e9a2b49147a8
44787 <44787@Tayyaba>
2022-10-31 21:44:00 +0000

    project created

---
+++ Test Suites/UpdateUser.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UpdateUser</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4362db9b-db2f-47d6-a4cc-2371f10e97fd</testSuiteGuid>
+</TestSuiteEntity>
